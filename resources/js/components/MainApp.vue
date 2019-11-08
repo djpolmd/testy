4bfe4ce184a6b438486427037e1055f329c4830e
djpolmd <djpolmd@gmail.com>
2019-11-09 01:01:08 +0200

    adding router

--- resources/js/components/MainApp.vue
+++ resources/js/components/MainApp.vue
@@ -6,7 +6,7 @@
                     <div class="card-header">Example Component</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        This is our component
                     </div>
                 </div>
             </div>
@@ -16,8 +16,7 @@
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
+            name:'main-app'
         }
     }
 </script>
